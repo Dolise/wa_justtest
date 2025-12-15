937a3fbf0317bad65234a7f5fe9208e5ff7310e8
Dmitry Tagincev <dmitrytagincev@redcollar.ru>
2025-12-15 16:27:42 +0300

    ыва

--- main.py
+++ main.py
@@ -670,7 +670,7 @@ def print_page_dump(driver):
 
 
 def main():
-    phone_number = "79810806416"
+    phone_number = "79940301649"
     avd_name = "Pixel_4_API_26"
     port = 5554
     device_name = MEMU_DEVICE if USE_MEMU else f"emulator-{port}"
